caed3d214bbab26596d966e0ef166596528a1709
Elyse Roth <elysemorganroth@gmail.com>
2021-10-19 16:14:49 -0400

    Add project files.

---
+++ PigDice/PigDice.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PigDice
+{
+    class PigDice
+    {
+        private Random rnd = new Random();
+
+        public int RunToHighScore(int highScore)
+        {
+            Console.WriteLine($"Begin: {DateTime.Now}");
+            var score = PlayPigDice();
+            var currHighScore = 0;
+            while(score < highScore)
+            {
+                score = PlayPigDice();
+                if(score > currHighScore)
+                {
+                    currHighScore = score;
+                    Console.WriteLine($"New high score is  {score}");
+                }
+            }
+            Console.WriteLine($"End: {DateTime.Now}");
+            return score;
+        }
+
+        public int PlayPigDice()
+        {
+            var score = 0;
+            var roll = RollDice();
+            while(roll != 1)
+            {
+                score += roll;
+                roll = RollDice();
+            }
+            return score;
+        }
+        private int RollDice()
+        {
+            return rnd.Next(1, 7);
+        }
+    }
+}
---
+++ PigDice/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace PigDice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var pd = new PigDice();
+            var score = pd.RunToHighScore(500);
+            
+            {
+                Console.WriteLine($"New high score is {score}");
+            }
+
+            //var highScore = 0;
+            //for(var idx = 0; idx < 100000; idx++) {
+            //    var score = pd.PlayPigDice();
+            //    if(score > highScore)
+            //    {
+            //        highScore = score;
+            //        Console.WriteLine($"Score is {score}");
+            //    }
+            //}
+        }
+    }
+}
